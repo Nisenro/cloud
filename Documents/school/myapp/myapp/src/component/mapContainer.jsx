5e099f8b33ea24d6630e853ad9217dd56696a87d
Adeseke <adegbenroadeseke@gmail.com>
2023-03-23 15:09:47 +0100

    final commit

--- Documents/school/myapp/myapp/src/component/mapContainer.jsx
+++ Documents/school/myapp/myapp/src/component/mapContainer.jsx
@@ -1,6 +1,5 @@
 import {GoogleMap, LoadScript, Marker} from "@react-google-maps/api"
 
-//googlemap helps us render the content in our component on Google maps while LOADSCRIPT loads the google maps api script and MARKER identifies a location on the map
 
 const MapContainer = () => {
   const item = {
